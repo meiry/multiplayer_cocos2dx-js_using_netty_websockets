1a085577add137e8880f1e38841f09a61af5136f
meiry <meiry242@gmail.com>
2016-01-20 21:44:04 +0200

    http://www.gamedevcraft.com/

---
+++ src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    sprites_png : "res/sprites.png",
+    sprites_plist : "res/sprites.plist",
+    grossini_plist : "res/grossini.plist",
+    grossini_png : "res/grossini.png",
+    
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
