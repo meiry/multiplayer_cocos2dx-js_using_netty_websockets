0059dcb902c976da39a9f642fcff0e5d68fffb3a
Meir Yanovich <meiry242@gmail.com>
2016-01-23 17:06:32 +0200

    http://www.gamedevcraft.com/
    http://www.gamedevcraft.com/

--- Server/CardWarNettyServer/src/com/server/GameServerMain.java
+++ Server/CardWarNettyServer/src/com/server/GameServerMain.java
@@ -2,8 +2,7 @@ package com.server;
 
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
-//http://comoyo.github.io/blog/2012/07/30/integrating-websockets-in-netty/
-//http://kevinwebber.ca/multiplayer-tic-tac-toe-in-java-using-the-websocket-api-netty-nio-and-jquery/
+ 
 public class GameServerMain {
 	private final static Logger LOG = LoggerManager.GetLogger(GameServerMain.class.getName());
 	public static void main(String[] args) {
